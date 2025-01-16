048846d39d1106e893f11a510a97a16cb671d854
Nejdet ALTINISIK <nejdet.altinisik@yahoo.fr>
2025-01-16 09:06:33 +0100

    Create RamFillWithIt.h

---
+++ RamFillWithIt.h
@@ -0,0 +1,9 @@
+//i want to use laforgue lathematician stuffs
+// and my previous stuffs on command line tiny os 
+//repository too.
+
+//it is best to start with a game. this will preve't us
+//to....
+class TryBestGame
+{
+}
